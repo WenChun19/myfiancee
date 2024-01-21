ec4b0ccb9e772afd075fab1c2e3b38835f21ec75
Hoh1996 <wenchun.sitegiant@gmail.com>
2024-01-21 21:58:56 +0800

    Add burgermenu

---
+++ app/components/BurgerMenu.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { HiBars3, HiMiniXMark } from "react-icons/hi2";
+import ThemeSwitch from "./ThemeSwitch";
+import Link from "next/link";
+import { menus } from "../libs/data";
+import { useState } from "react";
+
+const BurgerMenu = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <div className="sm:hidden flex items-center text-xl">
+        <HiBars3 className="cursor-pointer" onClick={() => setOpen(true)} />
+      </div>
+      <div
+        className={`absolute top-0 right-0 w-1/2 h-full
+       bg-gray-100 z-50 transition rounded-sm ${
+         open ? "flex" : "hidden"
+       } flex-col dark:bg-slate-800
+       p-3 pl-9 pt-7`}
+      >
+        <div className="flex justify-between items-center mb-2">
+          <ThemeSwitch />
+          <HiMiniXMark
+            className="text-xl cursor-pointer"
+            onClick={() => setOpen(false)}
+          />
+        </div>
+        <nav className="flex flex-col gap-3 mt-4 text-sm">
+          {menus.map((menu) => (
+            <>
+              <Link className="mr-2" key={menu.title} href={menu.href}>
+                {menu.title}
+              </Link>
+              <hr className="w-3/4" />
+            </>
+          ))}
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default BurgerMenu;
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,6 +3,10 @@ import Link from "next/link";
 import React from "react";
 import { menus } from "../libs/data";
 import ThemeSwitch from "./ThemeSwitch";
+import { Urbanist } from "next/font/google";
+import BurgerMenu from "./BurgerMenu";
+
+const urbanist = Urbanist({ subsets: ["latin"], weight: ["500"] });
 
 const Header = () => {
   return (
@@ -16,16 +20,20 @@ const Header = () => {
             <Image src="" width={30} height={30} alt="logo" />
           </span>
         </Link>
-        <div className="flex justify-around">
-          <>
-            {menus.map((menu) => (
-              <Link className="mr-2" key={menu.title} href={menu.href}>
-                {menu.title}
-              </Link>
-            ))}
-          </>
-          <ThemeSwitch />
+        <div
+          className={`hidden sm:flex items-center 
+          text-slate-600 justify-around ${urbanist.className}`}
+        >
+          {menus.map((menu) => (
+            <Link className="mr-2" key={menu.title} href={menu.href}>
+              {menu.title}
+            </Link>
+          ))}
+          <div className="flex items-center ml-3">
+            <ThemeSwitch />
+          </div>
         </div>
+        <BurgerMenu />
       </div>
     </nav>
   );
