ff598d886310b3ed8035845b01f5d2ff7ea376b4
Hoh1996 <wenchun.sitegiant@gmail.com>
2024-01-30 20:28:31 +0800

    Add generate static params

--- app/transaction/[id]/page.tsx
+++ app/transaction/[id]/page.tsx
@@ -2,11 +2,20 @@ import GoBack from "@/app/components/GoBack";
 import { TransactionDataType } from "@/app/components/ListingTable";
 import TransactionForm from "@/app/components/TransactionForm";
 import prisma from "@/app/libs/prismadb";
+import { notFound } from "next/navigation";
 
 type TransactionResponse = {
   status: string;
   transaction: TransactionDataType | null;
 };
+
+export async function generateStaticParams() {
+  const transactions = await prisma.transaction.findMany();
+
+  return transactions.map((transaction) => ({
+    id: transaction.id,
+  }));
+}
 
 const getTransaction = async (id: string): Promise<TransactionResponse> => {
   try {
@@ -25,6 +34,9 @@ const EditTransaction = async ({ params }: { params: { id: string } }) => {
   const { id } = params;
   const { transaction } = await getTransaction(id);
 
+  if (!transaction) {
+    notFound();
+  }
   return (
     <div className="flex flex-col m-auto w-3/4 p-8 my-3">
       <GoBack />
