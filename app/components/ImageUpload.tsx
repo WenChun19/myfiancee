93977e106be72f25a0c6173f78d66c631408f1ff
Hoh1996 <wenchun.sitegiant@gmail.com>
2024-02-05 21:32:29 +0800

    Add preview function

--- app/components/ImageUpload.tsx
+++ app/components/ImageUpload.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import React, { useRef, useState } from "react";
 import toast from "react-hot-toast";
+import { HiEye, HiMiniTrash, HiMiniXMark } from "react-icons/hi2";
 
 type ImageUploadProps = {
   handleUpload: (file: File) => void;
@@ -12,6 +13,7 @@ type ImageUploadProps = {
 const ImageUpload = ({ handleUpload, fileUrl }: ImageUploadProps) => {
   const imageRef = useRef<HTMLInputElement>(null);
   const [imageUrl, setImageUrl] = useState(fileUrl);
+  const [isPreview, setIsPreview] = useState(false);
 
   const handleClick = () => imageRef?.current?.click();
 
@@ -29,6 +31,18 @@ const ImageUpload = ({ handleUpload, fileUrl }: ImageUploadProps) => {
     handleUpload(file);
   };
 
+  const onRemoveImage = () => {
+    setImageUrl("");
+  };
+
+  const onOpenPreview = () => {
+    setIsPreview(true);
+  };
+
+  const onClosePreview = () => {
+    setIsPreview(false);
+  };
+
   return (
     <div className="flex flex-col my-2">
       <label htmlFor="file-input" className="mb-3">
@@ -38,11 +52,21 @@ const ImageUpload = ({ handleUpload, fileUrl }: ImageUploadProps) => {
         <div
           className="w-[120px]
           border-2 border-slate-400 aspect-square
-          relative"
+          relative group"
         >
+          <div
+            className="group-hover:z-30
+             flex gap-4  
+          absolute top-1/2 left-1/2 -translate-x-1/2
+          -translate-y-1/2 text-2xl cursor-pointer"
+          >
+            <HiEye onClick={onOpenPreview} />
+            <HiMiniTrash onClick={onRemoveImage} />
+          </div>
           <Image
             src={imageUrl}
             fill
+            className="group-hover:opacity-70"
             style={{ objectFit: "contain" }}
             alt="profile"
           />
@@ -65,6 +89,26 @@ const ImageUpload = ({ handleUpload, fileUrl }: ImageUploadProps) => {
             hidden
           />
           +
+        </div>
+      )}
+      {isPreview && (
+        <div
+          className="fixed inset-0 flex 
+      max-h-full bg-slate-50 z-40"
+        >
+          <div className="w-full flex justify-end p-2">
+            <HiMiniXMark
+              className="z-50 text-3xl
+            text-slate-900 cursor-pointer"
+            onClick={onClosePreview}
+            />
+          </div>
+          <Image
+            src={imageUrl || ""}
+            fill
+            style={{ objectFit: "contain" }}
+            alt="preview-profile"
+          />
         </div>
       )}
     </div>
